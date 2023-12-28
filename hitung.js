da603e4484880da5f5282ffa75cc7751b739c53f
RizalKubro <127368706+RizalKubro@users.noreply.github.com>
2023-12-28 20:18:32 +0700

    hitung.js

--- hitung.js
+++ hitung.js
@@ -1,62 +1,38 @@
+let barang = [
+    {kode : "001", nama: "A", harga: "10000"
+    },
+    {kode : "002", nama: "B", harga: "20000"
+    },
+    {kode : "003", nama: "C", harga: "30000"
+    }   
+]
 
-    // Data barang
-    let barang = [
-        {
-            kode: "001", 
-            nama: "A",
-            harga: 10000  
-        },
-        {
-            kode: "002", 
-            nama: "B", 
-            harga: 20000
-        },
-        {
-            kode: "003",
-            nama: "C", 
-            harga: 30000
+function cariBarang(){
+    for(let i = 0; i < barang.length; i++){
+        if(barang[i].kode == kode){
+            return barang[i]
         }
-        ];
-        
-        // Fungsi untuk mencari data barang
-        function cariBarang(kode) {
-        for(let i = 0; i < barang.length; i++) {
-            if(barang[i].kode === kode) {
-            return barang[i];
-            }
-        }
-        return null;
-        }
-        
-        function proses() {
-        
-          // Ambil data input
-        let kode = document.getElementById("kode").value;  
-        let jumlah = parseInt(document.getElementById("jumlah").value);
-        
-          // Cari data barang
-        let dataBarang = cariBarang(kode);
-        
-          // Jika data barang tidak ditemukan
-        if(!dataBarang) {
-            alert("Kode barang tidak ditemukan!");
-            return;  
-        }
-    
-          // Hitung total harga
-          let total = dataBarang.harga * jumlah;
-        
-          // Tampilkan total harga    
-        alert("Total yang harus dibayar: Rp." + total); 
-    
-          // Ambil input pembayaran
-        let bayar = parseInt(prompt("Uang yang dibayar:"));
-    
-          // Hitung kembalian
-        let kembalian = bayar - total;
-    
-          // Tampilkan kembalian
-        alert("Kembalian: Rp." + kembalian);
-        
-        }
-    
+    }
+    return null;
+}
+
+function proses(){
+    let kode = document.getElementById("kode").value;
+    let jumlah = parseInt(document.getElementById("jumlah").value);
+
+    let dataBarang = cariBarang(kodo);
+
+    if(!dataBarang){
+        alert("kode barang tidak ditemukan");
+        return;
+    }
+    let total = dataBarang.harga * jumlah;
+
+    alert("Total yang harus dibayar: RP"+total);
+
+    let bayar = parseInt(prompt("Uang yang dibayar"));
+
+    let kembalian = bayar - total;
+
+    alert("kembalian: Rp"+ kembalian)
+}
